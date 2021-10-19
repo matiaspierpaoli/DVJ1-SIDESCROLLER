fb781fe0719817e144f4c85a512126cacad8772f
matiaspierpaoli <matiaspierpaoli@gmail.com>
2021-10-19 12:02:19 -0300

    Añadida la opcion en menu de visualizacion de controles

--- Flappy Cloak/src/manager/gamemanager.cpp
+++ Flappy Cloak/src/manager/gamemanager.cpp
@@ -70,22 +70,27 @@ void GameManager::menuScreen()
 {
 	Rectangle onePlayerButton;
 	onePlayerButton.x = GetScreenWidth() / 100 * 2.5f;
-	onePlayerButton.y = GetScreenHeight() / 2;
+	onePlayerButton.y = GetScreenHeight() / 2 - 30;
 	onePlayerButton.height = 30;
 	onePlayerButton.width = 120;
 	Rectangle twoPlayersButton;
 	twoPlayersButton.x = GetScreenWidth() / 100 * 2.5f;
-	twoPlayersButton.y = GetScreenHeight() / 2 + 50;
+	twoPlayersButton.y = GetScreenHeight() / 2 + 20;
 	twoPlayersButton.height = 30;
 	twoPlayersButton.width = 150;
+	Rectangle controllesButton;
+	controllesButton.x = GetScreenWidth() / 100 * 2.5f;
+	controllesButton.y = (GetScreenHeight() / 2) + (GetScreenHeight() / 100 * 16.2f);
+	controllesButton.height = 30;
+	controllesButton.width = 190;
 	Rectangle creditsButton;
 	creditsButton.x = GetScreenWidth() / 100 * 2.5f;
-	creditsButton.y = (GetScreenHeight() / 2) + (GetScreenHeight() / 100 * 22.2f);
+	creditsButton.y = (GetScreenHeight() / 2) + (GetScreenHeight() / 100 * 28.0f);
 	creditsButton.height = 30;
 	creditsButton.width = 113;
 	Rectangle closeButton;
 	closeButton.x = GetScreenWidth() / 100 * 2.5f;
-	closeButton.y = (GetScreenHeight() / 2) + (GetScreenHeight() / 100 * 37.3f);
+	closeButton.y = (GetScreenHeight() / 2) + (GetScreenHeight() / 100 * 40.0f); //37.3
 	closeButton.height = 30;
 	closeButton.width = 81.25f;
 
@@ -96,24 +101,29 @@ void GameManager::menuScreen()
 		DrawText(FormatText("Flap"), 20, GetScreenHeight() / 10, 120, SKYBLUE);
 
 		if (CheckCollisionPointRec(GetMousePosition(), onePlayerButton))
-			DrawText(FormatText("1 Player"), 20, GetScreenHeight() / 2, 30, RED);
+			DrawText(FormatText("1 Player"), 20, GetScreenHeight() / 2 - 30, 30, RED);
 		else
-			DrawText(FormatText("1 Player"), 20, GetScreenHeight() / 2, 30, WHITE);
+			DrawText(FormatText("1 Player"), 20, GetScreenHeight() / 2 - 30, 30, WHITE);
 
 		if (CheckCollisionPointRec(GetMousePosition(), twoPlayersButton))
-			DrawText(FormatText("2 Players"), 20, GetScreenHeight() / 2 + 50, 30, RED);
+			DrawText(FormatText("2 Players"), 20, GetScreenHeight() / 2 + 20, 30, RED);
 		else
-			DrawText(FormatText("2 Players"), 20, GetScreenHeight() / 2 + 50, 30, WHITE);
+			DrawText(FormatText("2 Players"), 20, GetScreenHeight() / 2 + 20, 30, WHITE);
+
+		if (CheckCollisionPointRec(GetMousePosition(), controllesButton))
+			DrawText(FormatText("Key Bindings"), 20, (GetScreenHeight() / 2) + (GetScreenHeight() / 100 * 16.2f), 30, RED);
+		else
+			DrawText(FormatText("Key Bindings"), 20, (GetScreenHeight() / 2) + (GetScreenHeight() / 100 * 16.2f), 30, WHITE);
 
 		if (CheckCollisionPointRec(GetMousePosition(), creditsButton))
-			DrawText(FormatText("Credits"), 20, (GetScreenHeight() / 2) + 100, 30, RED);
+			DrawText(FormatText("Credits"), 20, (GetScreenHeight() / 2) + (GetScreenHeight() / 100 * 28.0f), 30, RED);
 		else
-			DrawText(FormatText("Credits"), 20, (GetScreenHeight() / 2) + 100, 30, WHITE);
+			DrawText(FormatText("Credits"), 20, (GetScreenHeight() / 2) + (GetScreenHeight() / 100 * 28.0f), 30, WHITE);
 
 		if (CheckCollisionPointRec(GetMousePosition(), closeButton))
-			DrawText(FormatText("Close"), 20, (GetScreenHeight() / 2) + 150, 30, RED);
+			DrawText(FormatText("Close"), 20, (GetScreenHeight() / 2) + (GetScreenHeight() / 100 * 40.0f), 30, RED);
 		else
-			DrawText(FormatText("Close"), 20, (GetScreenHeight() / 2) + 150, 30, WHITE);
+			DrawText(FormatText("Close"), 20, (GetScreenHeight() / 2) + (GetScreenHeight() / 100 * 40.0f), 30, WHITE);
 
 		DrawText(FormatText("v 0.3"), GetScreenWidth() - 50, 1, 20, WHITE);
 
@@ -130,6 +140,11 @@ void GameManager::menuScreen()
 			DrawText(FormatText("Created by:"), (GetScreenWidth() / 2 + 40), (GetScreenHeight() / 4) + 100, 30, WHITE);
 			DrawText(FormatText("Matias P. Karplus"), (GetScreenWidth() / 2 + 40), (GetScreenHeight() / 4) + 130, 30, WHITE);
 			DrawText(FormatText("Matias Pierpaoli"), (GetScreenWidth() / 2 + 40), (GetScreenHeight() / 4) + 160, 30, WHITE);
+		}
+
+		if (CheckCollisionPointRec(GetMousePosition(), controllesButton)) {
+			DrawText(FormatText("Player 1 jumps with Space"), (GetScreenWidth() / 2), (GetScreenHeight() / 4) + 20, 28, WHITE);
+			DrawText(FormatText("Player 2 jumps with Enter"), (GetScreenWidth() / 2), (GetScreenHeight() / 4) + 100, 28, WHITE);			
 		}
 
 		if (CheckCollisionPointRec(GetMousePosition(), closeButton) && IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
