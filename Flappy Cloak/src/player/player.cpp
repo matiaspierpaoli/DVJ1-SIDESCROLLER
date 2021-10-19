7a943241f82a012ee96e2db735ff8f0744c81126
HavensurgeSW <matias@karplus.com.ar>
2021-10-19 08:10:46 -0300

    Implemented animation frames

--- Flappy Cloak/src/player/player.cpp
+++ Flappy Cloak/src/player/player.cpp
@@ -7,7 +7,8 @@ Player::Player()
 	rec.width = 40;
 	rec.height = 40;
 	color = GREEN;
-	tex = LoadTexture("res/CloakGuy.png");
+	tex1 = LoadTexture("res/CGf1.png");
+	tex2 = LoadTexture("res/CGf2.png");
 }
 
 Player::~Player()
@@ -16,18 +17,24 @@ Player::~Player()
 
 void Player::movement()
 {
-	if (IsKeyDown(KEY_W))rec.y -= GetFrameTime() * 420;
-	if (IsKeyDown(KEY_S))rec.y += GetFrameTime() * 420;
-
 	if (IsKeyDown(KEY_SPACE))
+	{
 		rec.y -= GetFrameTime() * 300;
+		falling = true;
+	}
 	else
+	{
 		rec.y += GetFrameTime() * 170;
+		falling = false;
+	}
 }
 
 void Player::draw()
 {
-	DrawTexture(tex, rec.x, rec.y, WHITE);
+	if (falling)
+		DrawTexture(tex1, rec.x, rec.y, WHITE);
+	else
+		DrawTexture(tex2, rec.x, rec.y, WHITE);
 }
 
 void Player::reset()
--- Flappy Cloak/src/player/player.h
+++ Flappy Cloak/src/player/player.h
@@ -4,7 +4,9 @@ class Player {
 private:
 	Rectangle rec;
 	Color color;
-	Texture2D tex;
+	Texture2D tex1;
+	Texture2D tex2;
+	bool falling;
 public:
 	Player();
 	~Player();
